021fe2b414ed7b3f863aac50842defb24e335d62
noodle <nonomi2603@gmail.com>
2025-07-21 01:41:20 +0500

    Create model order

--- Jetqor kaspi api/Models/Order.cs
+++ Jetqor kaspi api/Models/Order.cs
@@ -1,6 +1,13 @@
-﻿namespace Jetqor_kaspi_api.Models;
-
-public class Order
+﻿public class Order
 {
-    
+    public int Id { get; set; }
+    public string KaspiOrderId { get; set; }
+    public string Code { get; set; }
+    public long CreationDate { get; set; }
+    public double TotalPrice { get; set; }
+    public string Status { get; set; }
+    public string State { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerPhone { get; set; }
+    public string Address { get; set; }
 }
